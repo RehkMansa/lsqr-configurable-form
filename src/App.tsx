097f0d2e932cd188a0acb76bcc7537cf0b4683fa
rehkmansa <godspowernathaniel25@gmail.com>
2023-03-23 19:01:24 +0100

    created first page view

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SyntheticEvent } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+	};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+	return (
+		<main>
+			<form
+				onSubmit={handleSubmit}
+				className="max-w-lg mx-auto p-3 gap-2 grid"
+			>
+				<input
+					type="text"
+					placeholder="Enter url"
+					className="px-8 py-2 w-full border rounded-md"
+				/>
+				<div>
+					<button
+						className="rounded-full px-10 py-2 border-2 border-spacing-2 border-double"
+						type="submit"
+					>
+						Submit
+					</button>
+				</div>
+			</form>
+		</main>
+	);
+};
 
-export default App
+export default App;
